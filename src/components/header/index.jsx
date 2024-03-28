98189d099d3089d1bd76706667e68f8927693f3d
Sven Dev <130911793+svendev2024@users.noreply.github.com>
2024-03-28 07:49:40 -0700

    implemented initial header structure

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,8 +6,10 @@ export default function Header(props) {
   return (
     <main className="flex flex-row items-center justify-between h-20 p-4 w-full">
       <div>Seven</div>
-      <button className="py-4 px-8 bg-[green]">Login</button>
-      <button className="py-4 px-8">Register</button>
+      <div className="flex flex-row justify-between">
+        <button className="py-4 px-8 bg-[green]">Login</button>
+        <button className="py-4 px-8">Register</button>
+      </div>
     </main>
   );
 }
