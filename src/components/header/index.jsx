83f6c5613595e8a982e7f2e80c8ff50d4e30a314
Sven Dev <130911793+svendev2024@users.noreply.github.com>
2024-03-29 08:50:55 -0700

    fixed style and header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,15 @@
 "use client";
 import React, { useState } from "react";
 
+import './header.css'
 export default function Header(props) {
   
   return (
     <main className="flex flex-row items-center justify-between h-20 p-4 w-full">
       <div className="ml-12 text-2xl">Seven</div>
       <div className="flex flex-row justify-between gap-12">
-        <button className="py-4 px-8 bg-[green]">Login</button>
-        <button className="py-4 px-8 bg-[green]">Register</button>
+        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Login</button>
+        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Register</button>
       </div>
     </main>
   );
