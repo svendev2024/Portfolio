6afed3587fec4febc879eb3ac681e550eb899e1e
Sven Dev <130911793+svendev2024@users.noreply.github.com>
2024-04-05 06:40:00 -0700

    implemented footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,11 +6,25 @@ import './footer.css'
 export default function Footer() {
   
   return (
-    <main className="flex flex-row items-center justify-between h-20 p-8 w-full bg-[black] border-b-4]">
-      <div className="ml-12 text-2xl text-[white]">Seven</div>
-      <div className="flex flex-row justify-between gap-12">
-        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Login</button>
-        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Register</button>
+    <main className="flex flex-row justify-between p-16 w-full bg-[black] border-t-4">
+      <div className="w-1/2 flex flex-col">
+        <div className="flex flex-row">
+          <div className="text-white w-1/3">Disocrd:</div>
+          <div className="text-white w-1/3">kenjiimai</div>
+        </div>
+        <div className="flex flex-row">
+          <div className="text-white w-1/3">Gmail:</div>
+          <div className="text-white w-1/3">kenjiimai123@gmail.com</div>
+        </div>
+        <div className="flex flex-row">
+          <div className="text-white w-1/3">Github:</div>
+          <div className="text-white w-1/3">https://github.com/svendev2024</div>
+        </div>
+      </div>
+
+      <div className="w-1/2 flex flex-col gap-4">
+        <h3 className="text-white">Contact me</h3>
+        <textarea rows={5} className="p-2"></textarea>
       </div>
     </main>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 
-import './footer.css'
+import './header.css'
 
 interface HeaderProps {
   
@@ -10,8 +10,12 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
   
   return (
-    <main className="flex flex-row items-center justify-between h-20 p-8 w-full bg-[black] border-b-4]">
-      
+    <main className="flex flex-row items-center justify-between h-20 p-8 w-full bg-[black] border-b-4">
+      <div className="ml-12 text-2xl text-[white]">Seven</div>
+      <div className="flex flex-row justify-between gap-12">
+        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Login</button>
+        <button className="py-4 px-8 bg-[#bebfbe] rounded auth">Register</button>
+      </div>
     </main>
   );
 }
